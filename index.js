54ec8c6c52cfb3bd68d04e02c9673030773d38a6
123angrybird <vuky9999@gmail.com>
2023-08-13 20:17:32 +1000

    add and subtract time features

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const Time = require("./server/class/time.js")
 
-require('dotenv').config();
-
-const app = express();
+require("dotenv").config();
+const host = process.env.HOST;
 const port = process.env.PORT;
 
+app = express();
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`))
+var current_time = new Time(22,0,"24-hour clock")
+
+
+console.log(current_time.subtract(10000));
+
+
+// app.get("/", function(req, res) {
+//     res.send("adsaadad");
+// })
+//app.listen(port, host, ()=> console.log(`Server is listening at ${host}:${port}`));
---
+++ server/class/time.js
@@ -0,0 +1,61 @@
+class Time {
+    constructor(hour, minute) {
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    setTime(hour = 0, minute = 0) {
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    whatTimeIsIt() {
+        return {
+            hour: this.hour,
+            minute: this.minute,
+            type: "24-hour clock"
+        }
+    }
+
+    to12HourClock() {
+        let time_12 = {
+            hour: this.hour,
+            minute: this.minute,
+            type: ""
+        }
+        if (time_12.hour >= 12) {
+            time_12.type = "pm";
+            time_12.hour = time_12.hour - 12;
+        } else {
+            time_12.type = "am";
+        }
+        if (time_12.hour == 0) {
+            time_12.hour = 12;
+        }
+        return time_12;
+    }
+
+    add(minute) {
+        let sum = minute + this.minute + this.hour * 60;
+
+        this.hour = Math.floor(sum/60)%24;
+        this.minute = sum % 60;
+
+        return this;
+    }
+
+    subtract(minute) {
+        let sum = this.minute + this.hour * 60 - minute;
+        
+        if (sum < 0) {
+            sum = 1440 + sum % 1440; 
+        }
+
+        this.hour = Math.floor(sum/60)%24;
+        this.minute = sum % 60;
+
+        return this;
+    }
+}
+
+module.exports = Time;
